bce19ef5e608d1ba75895aafd6efb7b681308bee
Right Fix <righteousnessude@gmail.com>
2025-09-01 20:00:31 +0100

    update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,26 @@
 import time
 import streamlit as st
 import pandas as pd
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
 from Bias import BiasClass
+
+
+
+# Authenticate
+gauth = GoogleAuth()
+gauth.LocalWebserverAuth()  # Opens browser for login
+drive = GoogleDrive(gauth)
+
+# Save to CSV locally
+file_name = "data.csv"
 
 st.title("Trade Bias")
 
 coins = sorted(["ETHUSDT", "AAVEUSDT", "SOLUSDT", "COMPUSDT", "BNBUSDT", "BTCUSDT", "BCHUSDT", "GNOUSDT"])
 BC = BiasClass(coins)
 
-df = pd.read_csv("dataset/bias_record.csv")
+df = pd.read_csv("bias_record.csv")
 df = df.drop_duplicates()
 data = { "Time": [],
     "Crypto_Currency" :[],
@@ -37,6 +49,11 @@ data = pd.DataFrame(data)
 new  = pd.concat([df, data], ignore_index = True)
 new.to_csv("dataset/bias_record.csv", index= False)   
 st.table(new)
+
+# Upload to Google Drive
+file = drive.CreateFile({'title': file_name})
+file.SetContentFile(file_name)
+file.Upload()
 
 time.sleep(60)
 st.rerun()
