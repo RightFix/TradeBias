1c505a3c51226359806976aa63a45fda161f216f
Right Fix <righteousnessude@gmail.com>
2025-09-01 17:13:03 +0100

    update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,16 @@
 import streamlit as st
 from Bias import BiasClass
 
+
+st.title("Trade Bias")
+
 coins = sorted(["ETHUSDT", "AAVEUSDT", "SOLUSDT", "COMPUSDT", "BNBUSDT", "BTCUSDT", "BCHUSDT", "GNOUSDT"])
 BC = BiasClass(coins)
 
 for i in range(len(coins)):
-  print(f"{coins[i]} bias score: {BC.bias_count(i)}")
+  st.write(f"{coins[i]} bias score: {BC.bias_count(i)}")
 
-st.title("🎈 My new app")
+
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
