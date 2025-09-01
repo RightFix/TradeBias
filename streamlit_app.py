20960805093f016ec7944d652563ba3280138db5
Right Fix <righteousnessude@gmail.com>
2025-09-01 17:13:20 +0100

    update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,3 @@ BC = BiasClass(coins)
 for i in range(len(coins)):
   st.write(f"{coins[i]} bias score: {BC.bias_count(i)}")
 
-
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
